4f79593d73975a1040ebe2f9d32b6cc58f6b227b
calvin <hbwang@bsbii.cn>
2020-01-03 13:12:50 +0800

    update docker, requirements

--- docker/slim/build.bash
+++ docker/slim/build.bash
@@ -3,6 +3,10 @@
 pushd $(pwd)
 SCRIPT_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
 cd ${SCRIPT_DIR}
+if [ ! -d sdk ]
+then
+mkdir sdk
+fi
 cp ../../dist/iqtestsdk-0.2.4.4.tar.gz sdk
 docker rm model:v-slim
 docker build -t model:v-slim .
